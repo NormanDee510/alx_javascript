29b7c88a2d264a06f9cdad9cae67c2b712249813
NormanDee <newman.demba@uhuruinnovative.com>
2023-09-02 14:15:33 -0700

    Object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value.replace(89);
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value.replace(12,89);
+console.log(myObject);
