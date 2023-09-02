972136e9473647898b7a2251f0c4137c80730ee3
NormanDee <newman.demba@uhuruinnovative.com>
2023-09-02 14:03:50 -0700

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const Languagearray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
 Languagearray.forEach(function (element) {
-    console.log(element);
+console.log(element);
 });
