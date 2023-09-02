06a892d0e62b41947a19605f84fc4116a5800b4e
NormanDee <newman.demba@uhuruinnovative.com>
2023-09-02 13:26:06 -0700

    3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myFirstVar = 'C is fun';
+const mySecondVar = 'Python is cool';
+const myThirdVar = 'JavaScript is amazing';
+
+console.log(myFirstVar);
+console.log(mySecondVar);
+console.log(myThirdVar);
