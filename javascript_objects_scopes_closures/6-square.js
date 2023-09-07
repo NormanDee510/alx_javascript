0abe6e31203fe850152a243d9bf549f0b8300a4a
newman <macnoedy@gmail.com>
2023-09-06 20:21:24 -0700

    Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,17 +2,9 @@
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
-  constructor(size) {
-    super(size); // Call the constructor of the BaseSquare class with size
-  }
-
-  charPrint(c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+  charPrint(c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
