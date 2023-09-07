d992a034bb50ab1438cb1b2f30a19069072f9238
newman <macnoedy@gmail.com>
2023-09-07 14:42:34 -0700

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) { 
-          this.width = w;
-          this.height = h; 
+          //this.width = w;
+          //this.height = h; 
 
         if (w > 0 && h > 0) {
             {}; 
