7799672ec9f04313e72b25063cdebc64495781c7
newman <macnoedy@gmail.com>
2023-09-06 20:36:49 -0700

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,8 @@ class Rectangle {
       if (w <= 0 || h <= 0) {
         return {}; 
       }
-      this.width = w;
-      this.height = h;
+      //this.width = w;
+      //this.height = h;
     }
   }
   
