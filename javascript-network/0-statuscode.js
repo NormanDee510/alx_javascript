3b1983ca416a0c08d6400c91fa5e71f1ed4fec67
newman <macnoedy@gmail.com>
2023-09-09 11:28:22 -0700

    status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,7 +1,7 @@
 const request = require('request');
 request.get('https://intranet.alxswe.com', function (error, response) {
 if (error) {
-    console.log(`Error: ${error.message}`)
+    console.log(`Error: ${error.statusCode}`)
 }else{
     console.log(`code: ${response.statusCode}`);
 }
