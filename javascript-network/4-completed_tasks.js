5182e3afa3ab3b4e1ee7b86f30e782db2d433428
newman <macnoedy@gmail.com>
2023-09-09 22:41:43 -0700

    How many completed?

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -2,15 +2,14 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
+  if (error) {   
     console.error(`Error: ${error.message}`);
-  } else if (response.statusCode !== 200) {
+  } else if (response.statusCode !== 200) {    
     console.error(`Error: Status Code ${response.statusCode}`);
   } else {
-    try {
-      const tasksData = JSON.parse(body);
-      const completedTaskCounts = {};
-
+    try {     
+      const tasksData = JSON.parse(body);      
+      const completedTaskCounts = {};     
       tasksData.forEach((task) => {
         if (task.completed) {
           if (completedTaskCounts[task.userId]) {
@@ -19,11 +18,9 @@ request.get(apiUrl, (error, response, body) => {
             completedTaskCounts[task.userId] = 1;
           }
         }
-      });
-
-      // Create an object and stringify it with no spaces
-      const formattedOutput = `{${JSON.stringify(completedTaskCounts, null, 2).replace(/"/g, "'").replace(/\n\s*/g, '')}}`;
-      
+      });     
+      const formattedOutput = JSON.stringify(completedTaskCounts, null, 2)
+      .replace(/"/g, "'");
       console.log(formattedOutput);
     } catch (parseError) {
       console.error(`Error parsing JSON: ${parseError.message}`);
