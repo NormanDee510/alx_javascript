c2f0cbb00b8f030d448ee767446125b9d403b59a
newman <macnoedy@gmail.com>
2023-09-09 17:03:46 -0700

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,24 +1,30 @@
-// Import the 'request' module
 const request = require('request');
 
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/'
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: Status Code ${response.statusCode}`);
-  } else {
-    try {
-      const filmsData = JSON.parse(body);
-      let filmCount = 0;
-      filmsData.results.forEach((film) => {
-        if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+let filmCount = 0;
+
+function fetchFilmData(url) {
+  request.get(url, (error, response, body) => {
+    if (error) { 
+      console.error(`Error: ${error.message}`);
+    } else if (response.statusCode !== 200) {     
+      console.error(`Error: Status Code ${response.statusCode}`);
+    } else {
+      try {       
+        const filmData = JSON.parse(body);        
+        if (filmData.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
           filmCount++;
+        }        
+        if (filmData.next) {         
+          fetchFilmData(filmData.next);
+        } else {        
+          console.log(filmCount);
         }
-      });
-      console.log(filmCount);
-    } catch (parseError) {
-      console.error(`Error parsing JSON: ${parseError.message}`);
+      } catch (parseError) {
+        console.error(`Error parsing JSON: ${parseError.message}`);
+      }
     }
-  }
-});
+  });
+}
+fetchFilmData(apiUrl);
