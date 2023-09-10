e73c374db804ad350708e43d3e9a648b085e74f2
newman <macnoedy@gmail.com>
2023-09-09 17:23:06 -0700

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,41 +1,41 @@
 const request = require('request');
 
 const apiUrlFilms = 'https://swapi-api.alx-tools.com/api/films/';
-const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-
+const characterId = 18;
 let filmCount = 0;
 
-function fetchCharacterData(url) {
-  request.get(url, (error, response, body) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-    } else if (response.statusCode !== 200) {
-      console.error(`Error: Status Code ${response.statusCode}`);
-    } else {
-      try {
-        const characterData = JSON.parse(body);
-        const filmAppearances = characterData.films;        
-        filmAppearances.forEach((filmUrl) => {
-          request.get(filmUrl, (filmError, filmResponse, filmBody) => {
-            if (filmError) {
-              console.error(`Error: ${filmError.message}`);
-            } else if (filmResponse.statusCode !== 200) {
-              console.error(`Error: Status Code ${filmResponse.statusCode}`);
-            } else {
+request.get(`https://swapi-api.alx-tools.com/api/people/${characterId}/`, (characterError, characterResponse, characterBody) => {
+  if (characterError) {
+    console.error(`Error: ${characterError.message}`);
+  } else if (characterResponse.statusCode !== 200) {
+    console.error(`Error: Status Code ${characterResponse.statusCode}`);
+  } else {
+    try {
+      const characterData = JSON.parse(characterBody);
+      const filmsAppearances = characterData.films;
+      filmsAppearances.forEach((filmUrl) => {       
+        request.get(filmUrl, (filmError, filmResponse, filmBody) => {
+          if (filmError) {
+            console.error(`Error: ${filmError.message}`);
+          } else if (filmResponse.statusCode !== 200) {
+            console.error(`Error: Status Code ${filmResponse.statusCode}`);
+          } else {
+            try {
               const filmData = JSON.parse(filmBody);
-              if (filmData.characters.includes(characterUrl)) {
+              if (filmData) {
                 filmCount++;
               }
+            } catch (parseError) {
+              console.error(`Error parsing JSON: ${parseError.message}`);
             }
-          });
+          }
         });
-      } catch (parseError) {
-        console.error(`Error parsing JSON: ${parseError.message}`);
-      }
+      });     
+      setTimeout(() => {
+        console.log(filmCount);
+      }, filmsAppearances.length * 1000);
+    } catch (parseError) {
+      console.error(`Error parsing JSON: ${parseError.message}`);
     }
-  });
-}
-fetchCharacterData(characterUrl);
-setTimeout(() => {
-  console.log(filmCount);
-}, 1000); 
+  }
+});
