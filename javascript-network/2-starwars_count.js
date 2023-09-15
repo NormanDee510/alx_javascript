71744484873835f84ad1a167d98efe935067d64e
newman <macnoedy@gmail.com>
2023-09-15 07:43:21 -0700

    starwars count

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,68 +1,23 @@
 const request = require('request');
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
 const characterId = 18;
 
-function fetchAllFilms(apiUrl) {
-  return new Promise((resolve, reject) => {
-    request.get(apiUrl, (error, response, body) => {
-      if (error) {
-        reject(`Error fetching films: ${error.message}`);
-      } else if (response.statusCode !== 200) {
-        reject(`Error fetching films: Status Code ${response.statusCode}`);
-      } else {
-        try {
-          const filmsData = JSON.parse(body).results;
-          resolve(filmsData);
-        } catch (parseError) {
-          reject(`Error parsing films JSON: ${parseError.message}`);
-        }
-      }
-    });
-  });
-}
-
-async function fetchCharacterData(characterUrl) {
-  return new Promise((resolve, reject) => {
-    request.get(characterUrl, (error, response, body) => {
-      if (error) {
-        reject(`Error fetching character: ${error.message}`);
-      } else if (response.statusCode !== 200) {
-        reject(`Error fetching character: Status Code ${response.statusCode}`);
-      } else {
-        try {
-          const characterData = JSON.parse(body);
-          resolve(characterData);
-        } catch (parseError) {
-          reject(`Error parsing character JSON: ${parseError.message}`);
-        }
-      }
-    });
-  });
-}
-
-async function countWedgeAntillesAppearances(apiUrl, characterId) {
-  try {
-    const films = await fetchAllFilms(apiUrl);
-    let count = 0;
-
+async function getWedgeAntillesMovieCount() {
+  try {   
+    const response = await request.get(apiURL);
+    const films = response.data.results;   
+    let wedgeAntillesMovieCount = 0;
     for (const film of films) {
-      const characters = await Promise.all(film.characters.map(fetchCharacterData));
-
-      if (characters.some((character) => character.url.endsWith(`people/${characterId}/`))) {
-        count++;
+      if (film.character_ids.includes(characterId)) {
+        wedgeAntillesMovieCount++;
       }
     }
 
-    return count;
+    console.log(`${wedgeAntillesMovieCount}`);
   } catch (error) {
-    throw error; // Rethrow the error for centralized error handling
+    console.error('Error fetching data from the API:', error.message);
   }
 }
 
-countWedgeAntillesAppearances(apiUrl, characterId)
-  .then((count) => {
-    console.log(count);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+getWedgeAntillesMovieCount();
