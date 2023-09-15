d802797333661c3d166858781733f5cbf437f479
newman <macnoedy@gmail.com>
2023-09-15 07:49:58 -0700

    starwars count

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -4,17 +4,23 @@ const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
 const characterId = 18;
 
 async function getWedgeAntillesMovieCount() {
-  try {   
+  try {
+    // Fetch data from the API
     const response = await request.get(apiURL);
-    const films = response.data.results;   
-    let wedgeAntillesMovieCount = 0;
-    for (const film of films) {
-      if (film.character_ids.includes(characterId)) {
-        wedgeAntillesMovieCount++;
-      }
-    }
 
-    console.log(`${wedgeAntillesMovieCount}`);
+    if (response.data && Array.isArray(response.data.results)) {
+      const films = response.data.results;     
+      let wedgeAntillesMovieCount = 0;     
+      for (const film of films) {
+        if (film.character_ids.includes(characterId)) {
+          wedgeAntillesMovieCount++;
+        }
+      }
+
+      console.log(`${wedgeAntillesMovieCount}`);
+    } else {
+      console.error('API response did not contain the expected data structure.');
+    }
   } catch (error) {
     console.error('Error fetching data from the API:', error.message);
   }
