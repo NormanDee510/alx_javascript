6f8b0ad1916a2144784e8c959d708c21fcf23aa1
newman <macnoedy@gmail.com>
2023-09-15 09:49:47 -0700

    starwars count

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,49 +1,26 @@
 const request = require('request');
-
-const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
+const apiUrl =  process.argv[2];
+//
 const characterId = 18;
-
-function getWedgeAntillesMovieCount() {
-  request(apiURL, (error, response, body) => {
-    if (error) {
-      console.error('Error fetching data from the API:', error.message);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`API returned status code ${response.statusCode}`);
-      return;
-    }
-
-    try {
-      const data = JSON.parse(body);
-
-      if (data && Array.isArray(data.results)) {
-        const films = data.results;
-        let count = 0;
-
-        // Define a regular expression to match the character URL ending with the character ID
-        const characterUrlRegex = new RegExp(`\\/people\\/${characterId}$`);
-
-        // Iterate through the films and check if Wedge Antilles is present
-        for (const film of films) {
-          if (film.characters.some((character) => character.match(characterUrlRegex))) {
-            count++;
-          }else if(film.characters.some((character) => character.match(!characterUrlRegex)))  {
-            count = 0;
-          } else{
-            count = 10;
-          }
-        }
-
-        console.log(`${count}`);
+  
+    request.get(apiUrl, (error, response, body) => {
+      if (error) {
+        console.error(`Error fetching films: ${error.message}`);
+      } else if (response.statusCode !== 200) {
+        console.error(`Error fetching films: Status Code ${response.statusCode}`);
       } else {
-        console.error('API response did not contain the expected data structure.');
+        try {
+          const filmsData = JSON.parse(body).results;
+          const filteredFilmWithWedge = filmsData.results.filter(function (film){
+            return film.characters.include(`/${characterId}/`);
+          })
+          console.log(filteredFilmWithWedge.length);
+        } catch (parseError) {
+          console.error(`Error parsing films JSON: ${parseError.message}`);
+        }
       }
-    } catch (parseError) {
-      console.error('Error parsing API response:', parseError.message);
-    }
-  });
-}
+    });
 
-getWedgeAntillesMovieCount();
+
+
+
