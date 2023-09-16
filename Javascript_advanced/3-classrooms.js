beb32181613d8a72e579386b91087ec9c6306f06
newman <macnoedy@gmail.com>
2023-09-16 13:01:21 -0700

    Closure and loops

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+const createClassRoom = (numbersOfStudents)=>{
+    function studentSeat (seat) {
+        return function () {
+            return seat;
+        }
+    }
+    var students = [];
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+var classRoom = createClassRoom(10);
+
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
