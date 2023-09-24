922dcae80f49738b7524361b371e21a99a336d9a
newman <macnoedy@gmail.com>
2023-09-23 20:17:30 -0700

    password and email

---
+++ html_js_validation/0-password-strength.js
@@ -0,0 +1,45 @@
+// Function to validate the password
+function validatePassword() {
+    const password = document.getElementById("password").value;
+    const errorElement = document.getElementById("error");
+
+    // Define regular expressions for each validation rule
+    const lengthRegex = /.{8,}/;
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const digitRegex = /[0-9]/;
+    const specialCharRegex = /[!@#$%^&*]/;
+
+    // Check each rule and display error messages as needed
+    if (!lengthRegex.test(password)) {
+        errorElement.textContent = "Password must be at least 8 characters long.";
+        return false;
+    }
+    if (!uppercaseRegex.test(password)) {
+        errorElement.textContent = "Password must contain at least one uppercase letter.";
+        return false;
+    }
+    if (!lowercaseRegex.test(password)) {
+        errorElement.textContent = "Password must contain at least one lowercase letter.";
+        return false;
+    }
+    if (!digitRegex.test(password)) {
+        errorElement.textContent = "Password must contain at least one numeric digit.";
+        return false;
+    }
+    if (!specialCharRegex.test(password)) {
+        errorElement.textContent = "Password must contain at least one special character (e.g., !@#$%^&*).";
+        return false;
+    }
+
+    // Clear any previous error messages and allow form submission
+    errorElement.textContent = "";
+    return true;
+}
+
+// Add an event listener to the form to call validatePassword on submit
+document.getElementById("passwordForm").addEventListener("submit", function (event) {
+    if (!validatePassword()) {
+        event.preventDefault(); // Prevent form submission if validation fails
+    }
+});
---
+++ html_js_validation/1-email-validation.js
@@ -0,0 +1,21 @@
+// Function to validate the email
+function validateEmail() {
+    const email = document.getElementById("email").value;
+    const errorElement = document.getElementById("error");    
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+    // Check if the email matches the regex pattern
+    if (!emailRegex.test(email)) {
+        errorElement.textContent = "Please enter a valid email address.";
+        return false;
+    }
+    // Clear any previous error messages and allow form submission
+    errorElement.textContent = "";
+    return true;
+}
+// Add an event listener to the form to call validateEmail on submit
+document.getElementById("emailForm").addEventListener("submit", function (event) {
+    if (!validateEmail()) {
+        event.preventDefault(); // Prevent form submission if validation fails
+    }
+});
